fa8f4700b416d58d4c2efa1ed25e128d9b813df8
AnastasiaStreltsova98 <anastasia.streltsova98@gmail.com>
2021-09-30 17:37:38 +1000

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float angle;
+    private AudioSource dogSound;
+
+    void Start()
+    {
+        dogSound = GetComponent<AudioSource>();
+    }
+
+    void DogSoundPlay ()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) 
+        {
+            dogSound.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            dogSound.Stop();
+        }
+    }
+
+    void Update()
+    {
+        Vector3 posOffset = gameObject.transform.position;
+        Vector3 rotOffset = Vector3.zero;
+        DogSoundPlay();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            posOffset += Time.deltaTime * speed * gameObject.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            posOffset -= Time.deltaTime * speed * gameObject.transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotOffset = new Vector3(0f, -Time.deltaTime * angle, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rotOffset = new Vector3(0f, Time.deltaTime * angle, 0f);
+        }
+
+        gameObject.transform.position = posOffset;
+        gameObject.transform.Rotate(rotOffset);
+
+    }
+}
